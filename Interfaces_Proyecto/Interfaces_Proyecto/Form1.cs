c9847596933a3469cd753030c8a338a2ad8a59f4
Abril Zayas <72897553+Abril2015@users.noreply.github.com>
2020-11-04 18:32:20 -0600

    Add files via upload

---
+++ Interfaces_Proyecto/Interfaces_Proyecto/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Interfaces_Proyecto
+{
+    public partial class PantallaInicio : Form
+    {
+        public PantallaInicio()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void PanelLogin_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PantallaInicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textUsuario_Enter(object sender, EventArgs e)
+        {
+            if (textUsuario.Text == "USUARIO")
+            {
+                textUsuario.Text = "";
+                textUsuario.ForeColor = Color.Maroon;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textUsuario_Leave(object sender, EventArgs e)
+        {
+            if (textUsuario.Text == "")
+            {
+                textUsuario.Text = "USUARIO";
+                textUsuario.ForeColor = Color.White;
+            }
+        }
+
+        private void textContraseña_Enter(object sender, EventArgs e)
+        {
+            if (textContraseña.Text == "CONTRASEÑA")
+            {
+                textContraseña.Text = "";
+                textContraseña.ForeColor = Color.Maroon;
+                textContraseña.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textContraseña_Leave(object sender, EventArgs e)
+        {
+            if (textContraseña.Text == "")
+            {
+                textContraseña.Text = "CONTRASEÑA";
+                textContraseña.ForeColor = Color.White;
+                textContraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void PantallaInicio_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
